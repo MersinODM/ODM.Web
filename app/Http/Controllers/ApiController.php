0dc5222e64e2002a2d550d90cf3af17973cc8c21
Hakan GÜLEN <hgulen33@gmail.com>
2020-11-16 12:54:36 +0300

    Apache License, Version 2.0 a geçildi

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,21 +1,19 @@
 <?php
 /*
+ * ODM.Web  https://github.com/electropsycho/ODM.Web
+ * Copyright 2019-2020 Hakan GÜLEN
  *
- *  * This file is part of the GPLv3 distribution (https://github.com/electropsycho/ODM.Web)
- *  * Copyright (c) 2015 Hakan GÜLEN
- *  *
- *  * This program is free software: you can redistribute it and/or modify
- *  * it under the terms of the GNU General Public License as published by
- *  * the Free Software Foundation, version 3.
- *  *
- *  * This program is distributed in the hope that it will be useful, but
- *  * WITHOUT ANY WARRANTY; without even the implied warranty of
- *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- *  * General Public License for more details.
- *  *
- *  * You should have received a copy of the GNU General Public License
- *  * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 namespace App\Http\Controllers;
